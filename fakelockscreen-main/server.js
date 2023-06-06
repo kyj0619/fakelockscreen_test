649d124777f946da71af1d4992dd8fddb30f88fb
kyj0619 <kyj97yj@gmail.com>
2023-06-06 16:55:10 +0900

    testing

---
+++ fakelockscreen-main/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+
+app.post('/api/capture', upload.single('image'), (req, res) => {
+  console.log(req.file);
+  res.json({ message: 'File uploaded successfully' });
+});
+
+
+app.listen(3000, () => console.log('Listening on port 3000'));
